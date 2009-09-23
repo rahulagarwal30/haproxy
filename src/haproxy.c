rahulagarwal30/haproxy

commit 96b06fb075c0fc906ab412307c5bc5bed00c8ae3

[MEDIUM] make it possible to change the buffer size in the configuration
The new tune.bufsize and tune.maxrewrite global directives allow one to
change the buffer size and the maxrewrite size. Right now, setting bufsize
too low will block stats sockets which will not be able to write at all.
An error checking must be added to buffer_write_chunk() so that if it
cannot write its message to an empty buffer, it causes the caller to abort.

src/haproxy.c
@@ -122,7 +122,11 @@ struct global global = {
 				 .mode = 0,
 			 }
 		 }
-	}
+	},
+	.tune = {
+		.bufsize = BUFSIZE,
+		.maxrewrite = MAXREWRITE,
+	},
 	/* others NULL OK */
 };
 
@@ -177,7 +181,10 @@ void display_build_opts()
 #ifdef BUILD_OPTIONS
 	       "\n  OPTIONS = " BUILD_OPTIONS
 #endif
-	       "\n\n");
+	       "\n\nDefault settings :"
+	       "\n  maxconn = %d, bufsize = %d, maxrewrite = %d, maxpollevents = %d"
+	       "\n\n",
+	       DEFAULT_MAXCONN, BUFSIZE, MAXREWRITE, MAX_POLL_EVENTS);
 }
 
 /*
@@ -603,6 +610,9 @@ void init(int argc, char **argv)
 
 	if (global.tune.recv_enough == 0)
 		global.tune.recv_enough = MIN_RECV_AT_ONCE_ENOUGH;
+
+	if (global.tune.maxrewrite >= global.tune.bufsize / 2)
+		global.tune.maxrewrite = global.tune.bufsize / 2;
 
 	if (arg_mode & (MODE_DEBUG | MODE_FOREGROUND)) {
 		/* command line debug mode inhibits configuration mode */

include/types/global.h
@@ -79,6 +79,8 @@ struct global {
 		int maxaccept;     /* max number of consecutive accept() */
 		int options;       /* various tuning options */
 		int recv_enough;   /* how many input bytes at once are "enough" */
+		int bufsize;       /* buffer size in bytes, defaults to BUFSIZE */
+		int maxrewrite;    /* buffer max rewrite size in bytes, defaults to MAXREWRITE */
 	} tune;
 	struct listener stats_sock; /* unix socket listener for statistics */
 	struct proxy *stats_fe;     /* the frontend holding the stats settings */

src/buffers.c
@@ -17,6 +17,7 @@
 #include <common/config.h>
 #include <common/memory.h>
 #include <proto/buffers.h>
+#include <types/global.h>
 
 struct pool_head *pool2_buffer;
 
@@ -24,7 +25,7 @@ struct pool_head *pool2_buffer;
 /* perform minimal intializations, report 0 in case of error, 1 if OK. */
 int init_buffer()
 {
-	pool2_buffer = create_pool("buffer", sizeof(struct buffer) + BUFSIZE, MEM_F_SHARED);
+	pool2_buffer = create_pool("buffer", sizeof(struct buffer) + global.tune.bufsize, MEM_F_SHARED);
 	return pool2_buffer != NULL;
 }
 

src/cfgparse.c
@@ -445,6 +445,26 @@ int cfg_parse_global(const char *file, int linenum, char **args, int kwm)
 		}
 		global.tune.maxaccept = atol(args[1]);
 	}
+	else if (!strcmp(args[0], "tune.bufsize")) {
+		if (*(args[1]) == 0) {
+			Alert("parsing [%s:%d] : '%s' expects an integer argument.\n", file, linenum, args[0]);
+			err_code |= ERR_ALERT | ERR_FATAL;
+			goto out;
+		}
+		global.tune.bufsize = atol(args[1]);
+		if (global.tune.maxrewrite >= global.tune.bufsize / 2)
+			global.tune.maxrewrite = global.tune.bufsize / 2;
+	}
+	else if (!strcmp(args[0], "tune.maxrewrite")) {
+		if (*(args[1]) == 0) {
+			Alert("parsing [%s:%d] : '%s' expects an integer argument.\n", file, linenum, args[0]);
+			err_code |= ERR_ALERT | ERR_FATAL;
+			goto out;
+		}
+		global.tune.maxrewrite = atol(args[1]);
+		if (global.tune.maxrewrite >= global.tune.bufsize / 2)
+			global.tune.maxrewrite = global.tune.bufsize / 2;
+	}
 	else if (!strcmp(args[0], "uid")) {
 		if (global.uid != 0) {
 			Alert("parsing [%s:%d] : user/uid already specified. Continuing.\n", file, linenum);
@@ -3482,13 +3502,13 @@ int cfg_parse_listen(const char *file, int linenum, char **args, int kwm)
 			goto out;
 		}
 
-		if (stat.st_size <= BUFSIZE) {
+		if (stat.st_size <= global.tune.bufsize) {
 			errlen = stat.st_size;
 		} else {
 			Warning("parsing [%s:%d] : custom error message file <%s> larger than %d bytes. Truncating.\n",
-				file, linenum, args[2], BUFSIZE);
+				file, linenum, args[2], global.tune.bufsize);
 			err_code |= ERR_WARN;
-			errlen = BUFSIZE;
+			errlen = global.tune.bufsize;
 		}
 
 		err = malloc(errlen); /* malloc() must succeed during parsing */

src/client.c
@@ -384,7 +384,7 @@ int event_accept(int fd) {
 		if ((s->req = pool_alloc2(pool2_buffer)) == NULL)
 			goto out_fail_req; /* no memory */
 
-		s->req->size = BUFSIZE;
+		s->req->size = global.tune.bufsize;
 		buffer_init(s->req);
 		s->req->prod = &s->si[0];
 		s->req->cons = &s->si[1];
@@ -393,7 +393,7 @@ int event_accept(int fd) {
 		s->req->flags |= BF_READ_ATTACHED; /* the producer is already connected */
 
 		if (p->mode == PR_MODE_HTTP) { /* reserve some space for header rewriting */
-			s->req->max_len -= MAXREWRITE;
+			s->req->max_len -= global.tune.maxrewrite;
 			s->req->flags |= BF_READ_DONTWAIT; /* one read is usually enough */
 		}
 
@@ -411,7 +411,7 @@ int event_accept(int fd) {
 		if ((s->rep = pool_alloc2(pool2_buffer)) == NULL)
 			goto out_fail_rep; /* no memory */
 
-		s->rep->size = BUFSIZE;
+		s->rep->size = global.tune.bufsize;
 		buffer_init(s->rep);
 		s->rep->prod = &s->si[1];
 		s->rep->cons = &s->si[0];

src/proto_uxst.c
@@ -482,7 +482,7 @@ int uxst_event_accept(int fd) {
 		if ((s->req = pool_alloc2(pool2_buffer)) == NULL)
 			goto out_free_task;
 
-		s->req->size = BUFSIZE;
+		s->req->size = global.tune.bufsize;
 		buffer_init(s->req);
 		s->req->prod = &s->si[0];
 		s->req->cons = &s->si[1];
@@ -499,7 +499,7 @@ int uxst_event_accept(int fd) {
 		if ((s->rep = pool_alloc2(pool2_buffer)) == NULL)
 			goto out_free_req;
 
-		s->rep->size = BUFSIZE;
+		s->rep->size = global.tune.bufsize;
 		buffer_init(s->rep);
 
 		s->rep->prod = &s->si[1];

src/session.c
@@ -330,7 +330,7 @@ void sess_establish(struct session *s, struct stream_interface *si)
 		}
 	}
 	else {
-		buffer_set_rlim(rep, req->size - MAXREWRITE); /* rewrite needed */
+		buffer_set_rlim(rep, req->size - global.tune.maxrewrite); /* rewrite needed */
 		s->txn.rsp.msg_state = HTTP_MSG_RPBEFORE;
 		/* reset hdr_idx which was already initialized by the request.
 		 * right now, the http parser does it.

commit aa9fb410c1d3308c203e17e2897b6341b49ab9a7

[BUG] variable buffer size ignored at initialization time
Commit 96b06fb075c0fc906ab412307c5bc5bed00c8ae3 introduced the ability
to configure buffer sizes. Unfortunately, the pool was created before
the conf was read, so that is was always set to the default size.

In order to fix that, we delay the call to init_buffer(), which is not
a problem since nothing uses it during the initialization.

src/haproxy.c
@@ -404,7 +404,7 @@ void init(int argc, char **argv)
 	signal_init();
 	init_task();
 	init_session();
-	init_buffer();
+	/* warning, we init buffers later */
 	init_pendconn();
 	init_proto_http();
 
@@ -555,6 +555,9 @@ void init(int argc, char **argv)
 		qfprintf(stdout, "Configuration file is valid\n");
 		exit(0);
 	}
+
+	/* now we know the buffer size, we can initialize the buffers */
+	init_buffer();
 
 	if (have_appsession)
 		appsession_init();