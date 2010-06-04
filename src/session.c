rahulagarwal30/haproxy

commit e323ea5dfcb0710507104416c2e2fd311757c6f3

[MAJOR] session: don't clear buffer status flags anymore
We want to be able to keep information about errors and timeouts
as long as possible in the buffer. Let's not clear these flags
anymore and keep them static. This does not seem to cause any
trouble, though a finer review might be wise.

src/session.c
@@ -778,8 +778,6 @@ resync_stream_interface:
 			sess_set_term_flags(s);
 		}
 
-		s->req->flags &= BF_CLEAR_READ & BF_CLEAR_WRITE & BF_CLEAR_TIMEOUT;
-		flags &= BF_CLEAR_READ & BF_CLEAR_WRITE & BF_CLEAR_TIMEOUT;
 		if ((s->req->flags ^ flags) & BF_MASK_STATIC)
 			resync = 1;
 	}
@@ -901,8 +899,6 @@ resync_stream_interface:
 	if ((s->req->flags ^ rqf_last) & BF_MASK_STATIC)
 		resync = 1;
 
-	s->req->flags &= BF_CLEAR_READ & BF_CLEAR_WRITE & BF_CLEAR_TIMEOUT;
-
 	/* according to benchmarks, it makes sense to resync now */
 	if (s->req->prod->state == SI_ST_DIS || s->req->cons->state == SI_ST_DIS)
 		goto resync_stream_interface;
@@ -925,8 +921,7 @@ resync_stream_interface:
 		    !(s->rep->flags & BF_FULL)) {
 			s->rep->hijacker(s, s->rep);
 		}
-		s->rep->flags &= BF_CLEAR_READ & BF_CLEAR_WRITE & BF_CLEAR_TIMEOUT;
-		flags &= BF_CLEAR_READ & BF_CLEAR_WRITE & BF_CLEAR_TIMEOUT;
+
 		if ((s->rep->flags ^ flags) & BF_MASK_STATIC)
 			resync = 1;
 	}
@@ -956,8 +951,6 @@ resync_stream_interface:
 			sess_set_term_flags(s);
 		}
 
-		s->rep->flags &= BF_CLEAR_READ & BF_CLEAR_WRITE & BF_CLEAR_TIMEOUT;
-		flags &= BF_CLEAR_READ & BF_CLEAR_WRITE & BF_CLEAR_TIMEOUT;
 		if ((s->rep->flags ^ flags) & BF_MASK_STATIC)
 			resync = 1;
 	}
@@ -1044,8 +1037,6 @@ resync_stream_interface:
 	if ((s->rep->flags ^ rpf_last) & BF_MASK_STATIC)
 		resync = 1;
 
-	s->rep->flags &= BF_CLEAR_READ & BF_CLEAR_WRITE & BF_CLEAR_TIMEOUT;
-
 	if (s->req->prod->state == SI_ST_DIS || s->req->cons->state == SI_ST_DIS)
 		goto resync_stream_interface;
 
@@ -1096,8 +1087,8 @@ resync_stream_interface:
 		if (s->req->cons->state == SI_ST_EST)
 			stream_sock_data_finish(s->req->cons);
 
-		s->req->flags &= BF_CLEAR_READ & BF_CLEAR_WRITE & BF_CLEAR_TIMEOUT;
-		s->rep->flags &= BF_CLEAR_READ & BF_CLEAR_WRITE & BF_CLEAR_TIMEOUT;
+		s->req->flags &= ~(BF_READ_NULL|BF_READ_PARTIAL|BF_WRITE_NULL|BF_WRITE_PARTIAL);
+		s->rep->flags &= ~(BF_READ_NULL|BF_READ_PARTIAL|BF_WRITE_NULL|BF_WRITE_PARTIAL);
 		s->si[0].prev_state = s->si[0].state;
 		s->si[1].prev_state = s->si[1].state;
 		s->si[0].flags &= ~(SI_FL_ERR|SI_FL_EXP);

commit bb3f25fbb0e5a544b622a36af6d8d24ace46bd6b

[BUG] session: clear BF_READ_ATTACHED before next I/O
The BF_READ_ATTACHED flag was created to wake analysers once after
a connection was established. It turns out that this flag is never
cleared once set, so even if there is no event, some analysers are
still evaluated for no reason.

The bug was introduced with commit e323ea5dfcb0710507104416c2e2fd311757c6f3.
It may cause slightly increased CPU usages during data transfers, maybe
even quite noticeable once when transferring transfer-encoded data,
due to the fact that the request analysers are being checked for every
chunk.

This fix must be backported in 1.4 after all non-reg tests have been
completed.

src/session.c
@@ -1521,8 +1521,8 @@ resync_stream_interface:
 		if (s->req->cons->state == SI_ST_EST && !s->req->cons->iohandler)
 			s->req->cons->update(s->req->cons);
 
-		s->req->flags &= ~(BF_READ_NULL|BF_READ_PARTIAL|BF_WRITE_NULL|BF_WRITE_PARTIAL);
-		s->rep->flags &= ~(BF_READ_NULL|BF_READ_PARTIAL|BF_WRITE_NULL|BF_WRITE_PARTIAL);
+		s->req->flags &= ~(BF_READ_NULL|BF_READ_PARTIAL|BF_WRITE_NULL|BF_WRITE_PARTIAL|BF_READ_ATTACHED);
+		s->rep->flags &= ~(BF_READ_NULL|BF_READ_PARTIAL|BF_WRITE_NULL|BF_WRITE_PARTIAL|BF_READ_ATTACHED);
 		s->si[0].prev_state = s->si[0].state;
 		s->si[1].prev_state = s->si[1].state;
 		s->si[0].flags &= ~(SI_FL_ERR|SI_FL_EXP);