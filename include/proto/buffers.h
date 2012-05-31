rahulagarwal30/haproxy

commit 13e66dad264e26e19f2f36037bafae8dee304c18

MINOR: buffers: add a rewind function
b_rew() will be used to rewind a buffer for certain specific operations
such as header inspection on data already in the output queue.

include/proto/buffers.h
@@ -303,6 +303,19 @@ static inline void b_adv(struct buffer *b, unsigned int adv)
 	b->p = b_ptr(b, adv);
 }
 
+/* Rewinds the buffer by <adv> bytes, which means that the buffer pointer goes
+ * backwards, and that as many bytes from out are moved to in. The caller is
+ * responsible for ensuring that adv is always smaller than or equal to b->o.
+ */
+static inline void b_rew(struct buffer *b, unsigned int adv)
+{
+	b->i += adv;
+	b->o -= adv;
+	if (!b->o && !b->pipe)
+		b->flags |= BF_OUT_EMPTY;
+	b->p = b_ptr(b, -adv);
+}
+
 /* Return the amount of bytes that can be written into the buffer at once,
  * excluding the amount of reserved space passed in <res>, which is
  * preserved.

commit 1bb6662a61ac6c19d3fa3b6644292c51a49cb858

BUG/MAJOR: b_rew() must pass a signed offset to b_ptr()
Commit 13e66da introduced b_rew() but passes -adv which is an unsigned
quantity on 64-bit platforms, causing the buffer to advance in the wrong
direction.

No backport is needed.

include/proto/buffers.h
@@ -313,7 +313,7 @@ static inline void b_rew(struct buffer *b, unsigned int adv)
 	b->o -= adv;
 	if (!b->o && !b->pipe)
 		b->flags |= BF_OUT_EMPTY;
-	b->p = b_ptr(b, -adv);
+	b->p = b_ptr(b, (int)-adv);
 }
 
 /* Return the amount of bytes that can be written into the buffer at once,