rahulagarwal30/haproxy

commit 2ab88675ecbf960a6f33ffe9c6a27f264150b201

MINOR: ssl: compare server certificate names to the SNI on outgoing connections
When support for passing SNI to the server was added in 1.6-dev3, there
was no way to validate that the certificate presented by the server would
really match the name requested in the SNI, which is quite a problem as
it allows other (valid) certificates to be presented instead (when hitting
the wrong server or due to a man in the middle).

This patch adds the missing check against the value passed in the SNI.
The "verifyhost" value keeps precedence if set. If no SNI is used and
no verifyhost directive is specified, then the certificate name is not
checked (this is unchanged).

In order to extract the SNI value, it was necessary to make use of
SSL_SESSION_get0_hostname(), which appeared in openssl 1.1.0. This is
a trivial function which returns the value of s->tlsext_hostname, so
it was provided in the compat layer for older versions. After some
refinements from Emmanuel, it now builds with openssl 1.0.2, openssl
1.1.0 and boringssl. A test file was provided to ease testing all cases.

After some careful observation period it may make sense to backport
this to 1.7 and 1.6 as some users rightfully consider this limitation
as a bug.

Cc: Emmanuel Hocdet <manu@gandi.net>
Signed-off-by: Willy Tarreau <w@1wt.eu>

src/ssl_sock.c
@@ -3926,7 +3926,7 @@ static int ssl_sock_srv_verifycbk(int ok, X509_STORE_CTX *ctx)
 {
 	SSL *ssl;
 	struct connection *conn;
-	char *servername;
+	const char *servername;
 
 	int depth;
 	X509 *cert;
@@ -3941,7 +3941,20 @@ static int ssl_sock_srv_verifycbk(int ok, X509_STORE_CTX *ctx)
 	ssl = X509_STORE_CTX_get_ex_data(ctx, SSL_get_ex_data_X509_STORE_CTX_idx());
 	conn = SSL_get_app_data(ssl);
 
+	/* we're checking against the configured "verifyhost" directive if
+	 * present, or against the SNI used on this connection if present.
+	 * If neither is set, the verification is OK.
+	 */
 	servername = objt_server(conn->target)->ssl_ctx.verify_host;
+	if (!servername) {
+		SSL_SESSION *ssl_sess = SSL_get_session(conn->xprt_ctx);
+		if (!ssl_sess)
+			return ok;
+
+		servername = SSL_SESSION_get0_hostname(ssl_sess);
+		if (!servername)
+			return ok;
+	}
 
 	/* We only need to verify the CN on the actual server cert,
 	 * not the indirect CAs */
@@ -4138,7 +4151,7 @@ int ssl_sock_prepare_srv_ctx(struct server *srv)
 	}
 	SSL_CTX_set_verify(srv->ssl_ctx.ctx,
 	                   verify,
-	                   srv->ssl_ctx.verify_host ? ssl_sock_srv_verifycbk : NULL);
+	                   (srv->ssl_ctx.verify_host || (verify & SSL_VERIFY_PEER)) ? ssl_sock_srv_verifycbk : NULL);
 	if (verify & SSL_VERIFY_PEER) {
 		if (srv->ssl_ctx.ca_file) {
 			/* load CAfile to verify */

include/proto/openssl-compat.h
@@ -94,6 +94,11 @@ static inline int SSL_SESSION_set1_id_context(SSL_SESSION *s, const unsigned cha
  * Functions introduced in OpenSSL 1.1.0 and not yet present in LibreSSL
  */
 
+static inline const char *SSL_SESSION_get0_hostname(const SSL_SESSION *sess)
+{
+       return sess->tlsext_hostname;
+}
+
 static inline const unsigned char *SSL_SESSION_get0_id_context(const SSL_SESSION *sess, unsigned int *sid_ctx_length)
 {
 	*sid_ctx_length = sess->sid_ctx_length;

tests/test-srv-verify.cfg
@@ -1,0 +1,57 @@
+global
+	maxconn 490
+	stats socket /tmp/sock1 mode 666 level admin
+	stats timeout 10m
+	ssl-server-verify none
+	tune.ssl.default-dh-param 1024
+	log /dev/log local0 debug info
+
+defaults
+	mode http
+	log     global
+	option  httplog
+	option  dontlognull
+	timeout connect 5s
+	timeout http-keep-alive 15s
+	timeout http-request 15s
+	timeout queue 30s
+	timeout tarpit 1m
+	timeout tunnel 300s
+	timeout client 30s
+	timeout server 60s
+
+listen 1
+	bind :8001
+
+	# passes checks and traffic (no hostname check)
+	# server ssl 127.0.0.1:8443 ssl verify required check inter 500 ca-file rsa2048.pem
+
+	# passes checks and traffic (localhost is what the server presents)
+	# server ssl 127.0.0.1:8443 ssl verify required check inter 500 ca-file rsa2048.pem verifyhost localhost
+
+	# fails checks and traffic (foo not matched on the server)
+	# server ssl 127.0.0.1:8443 ssl verify required check inter 500 ca-file rsa2048.pem verifyhost foo
+
+	# passes checks and traffic (verify none ignores the host)
+	# server ssl 127.0.0.1:8443 ssl verify none check inter 500 ca-file rsa2048.pem verifyhost foo
+
+	# passes checks and traffic (localhost is fine)
+	# server ssl 127.0.0.1:8443 ssl verify required check inter 500 ca-file rsa2048.pem sni str(localhost) verifyhost localhost
+
+	# passes checks and traffic (verifyhost overrides sni)
+	# server ssl 127.0.0.1:8443 ssl verify required check inter 500 ca-file rsa2048.pem sni str(foo) verifyhost localhost
+
+	# passes checks and traffic (localhost always valid)
+	# server ssl 127.0.0.1:8443 ssl verify required check inter 500 ca-file rsa2048.pem sni str(localhost)
+
+	# passes checks, and traffic without host or with "host: localhost" and fails other hosts.
+	server ssl 127.0.0.1:8443 ssl verify required check inter 500 ca-file rsa2048.pem sni req.hdr(host)
+
+	# just for tests
+	#server clear 127.0.0.1:8480
+
+listen 2
+	bind :8480
+	bind :8443 ssl crt rsa2048.pem
+	stats uri /
+

commit 853b29b0a6e25df6bfe433a192f9deb1c1a242e4

BUG/MINOR: ssl: make use of the name in SNI before verifyhost
Commit 2ab8867 ("MINOR: ssl: compare server certificate names to the SNI
on outgoing connections") introduced the ability to check server cert
names against the name provided with in the SNI, but verifyhost was kept
as a way to force the name to check against. This was a mistake, because :
  - if an SNI is used, any static hostname in verifyhost will be wrong ;
    worse, if it matches and doesn't match the SNI, the server presented
    the wrong certificate ;

  - there's no way to have a default name to check against for health
    checks anymore because the point above mandates the removal of the
    verifyhost directive

This patch reverses the ordering of the check : whenever SNI is used, the
name provided always has precedence (ie the server must always present a
certificate that matches the requested name). And if no SNI is provided,
then verifyhost is used, and will be configured to match the server's
default certificate name. This will work both when SNI is not used and
for health checks.

If the commit 2ab8867 is backported in 1.7 and/or 1.6, this one must be
backported too.

src/ssl_sock.c
@@ -3945,13 +3945,15 @@ static int ssl_sock_srv_verifycbk(int ok, X509_STORE_CTX *ctx)
 	ssl = X509_STORE_CTX_get_ex_data(ctx, SSL_get_ex_data_X509_STORE_CTX_idx());
 	conn = SSL_get_app_data(ssl);
 
-	/* we're checking against the configured "verifyhost" directive if
-	 * present, or against the SNI used on this connection if present.
-	 * If neither is set, the verification is OK.
+	/* We're checking if the provided hostnames match the desired one. The
+	 * desired hostname comes from the SNI we presented if any, or if not
+	 * provided then it may have been explicitly stated using a "verifyhost"
+	 * directive. If neither is set, we don't care about the name so the
+	 * verification is OK.
 	 */
-	servername = objt_server(conn->target)->ssl_ctx.verify_host;
+	servername = SSL_get_servername(conn->xprt_ctx, TLSEXT_NAMETYPE_host_name);
 	if (!servername) {
-		servername = SSL_get_servername(conn->xprt_ctx, TLSEXT_NAMETYPE_host_name);
+		servername = objt_server(conn->target)->ssl_ctx.verify_host;
 		if (!servername)
 			return ok;
 	}