rahulagarwal30/haproxy

commit fa8a52983bb68dcbc802a9e07556ae9f65bdbc4e

MINOR: standard: Add str2mask6 function
This new function mirrors the str2mask() function for IPv4 addresses.

This commit is in preparation to support ARGT_MSK6.

include/common/standard.h
@@ -377,6 +377,12 @@ struct sockaddr_storage *str2sa_range(const char *str,
  */
 int str2mask(const char *str, struct in_addr *mask);
 
+/* converts <str> to a struct in6_addr containing a network mask. It can be
+ * passed in quadruplet form (ffff::ffff::) or in CIDR form (64). It returns 1
+ * if the conversion succeeds otherwise zero.
+ */
+int str2mask6(const char *str, struct in6_addr *mask);
+
 /* convert <cidr> to struct in_addr <mask>. It returns 1 if the conversion
  * succeeds otherwise non-zero.
  */

src/standard.c
@@ -1028,6 +1028,28 @@ int str2mask(const char *str, struct in_addr *mask)
 	return 1;
 }
 
+/* converts <str> to a struct in6_addr containing a network mask. It can be
+ * passed in quadruplet form (ffff::ffff::) or in CIDR form (64). It returns 1
+ * if the conversion succeeds otherwise zero.
+ */
+int str2mask6(const char *str, struct in6_addr *mask)
+{
+	if (strchr(str, ':') != NULL) {	    /* quadruplet notation */
+		if (!inet_pton(AF_INET6, str, mask))
+			return 0;
+	}
+	else { /* mask length */
+		char *err;
+		unsigned long len = strtol(str, &err, 10);
+
+		if (!*str || (err && *err) || (unsigned)len > 128)
+			return 0;
+
+		len2mask6(len, mask);
+	}
+	return 1;
+}
+
 /* convert <cidr> to struct in_addr <mask>. It returns 1 if the conversion
  * succeeds otherwise zero.
  */

commit d363525142ff5c81b9b1a69d3e2d01dd7e3c9675

CLEANUP: standard: Fix typo in IPv6 mask example
IPv6 addresses with two double colons are invalid.

This typo was introduced in commit fa8a52983bb68dcbc802a9e07556ae9f65bdbc4e.

include/common/standard.h
@@ -378,7 +378,7 @@ struct sockaddr_storage *str2sa_range(const char *str,
 int str2mask(const char *str, struct in_addr *mask);
 
 /* converts <str> to a struct in6_addr containing a network mask. It can be
- * passed in quadruplet form (ffff::ffff::) or in CIDR form (64). It returns 1
+ * passed in quadruplet form (ffff:ffff::) or in CIDR form (64). It returns 1
  * if the conversion succeeds otherwise zero.
  */
 int str2mask6(const char *str, struct in6_addr *mask);

src/standard.c
@@ -1037,7 +1037,7 @@ int str2mask(const char *str, struct in_addr *mask)
 }
 
 /* converts <str> to a struct in6_addr containing a network mask. It can be
- * passed in quadruplet form (ffff::ffff::) or in CIDR form (64). It returns 1
+ * passed in quadruplet form (ffff:ffff::) or in CIDR form (64). It returns 1
  * if the conversion succeeds otherwise zero.
  */
 int str2mask6(const char *str, struct in6_addr *mask)