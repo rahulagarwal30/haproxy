rahulagarwal30/haproxy

commit 0b3e849a48a63c09fdcbe26a36895a6f03afb34d

MEDIUM: listeners: set O_CLOEXEC on the accepted FDs
Set the O_CLOEXEC flag on the accept, useful to avoid an FD leak in the
master process, since it reexecutes itself during a reload

src/listener.c
@@ -545,7 +545,7 @@ void listener_accept(int fd)
 		/* with sockpair@ we don't want to do an accept */
 		if (unlikely(l->addr.ss_family == AF_CUST_SOCKPAIR)) {
 			if ((cfd = recv_fd_uxst(fd)) != -1)
-				fcntl(cfd, F_SETFL, O_NONBLOCK);
+				fcntl(cfd, F_SETFL, O_NONBLOCK|O_CLOEXEC);
 		} else
 
 #ifdef USE_ACCEPT4
@@ -553,12 +553,12 @@ void listener_accept(int fd)
 		 * fallback to the legacy accept() + fcntl().
 		 */
 		if (unlikely(accept4_broken ||
-			((cfd = accept4(fd, (struct sockaddr *)&addr, &laddr, SOCK_NONBLOCK)) == -1 &&
+			((cfd = accept4(fd, (struct sockaddr *)&addr, &laddr, SOCK_NONBLOCK|SOCK_CLOEXEC)) == -1 &&
 			(errno == ENOSYS || errno == EINVAL || errno == EBADF) &&
 			(accept4_broken = 1))))
 #endif
 			if ((cfd = accept(fd, (struct sockaddr *)&addr, &laddr)) != -1)
-				fcntl(cfd, F_SETFL, O_NONBLOCK);
+				fcntl(cfd, F_SETFL, O_NONBLOCK|O_CLOEXEC);
 
 		if (unlikely(cfd == -1)) {
 			switch (errno) {

commit 9b3b5e02bcca8313bba21fccf132fa173339498f

BUG/MEDIUM: listeners: CLOEXEC flag is not correctly set
The CLOEXEC flag was set using a F_SETFL which can't work.
To set the CLOEXEC flag F_SETFD should be used, the problem is that it
needs a new call to fcntl() and it's on the path of every accept.

This flag was only needed in the case of the master, so the patch was
reverted and the flag set only in this case.

The bug was introduced by 0b3e849 ("MEDIUM: listeners: set O_CLOEXEC on
the accepted FDs").

No backport needed.

src/listener.c
@@ -544,7 +544,7 @@ void listener_accept(int fd)
 		/* with sockpair@ we don't want to do an accept */
 		if (unlikely(l->addr.ss_family == AF_CUST_SOCKPAIR)) {
 			if ((cfd = recv_fd_uxst(fd)) != -1)
-				fcntl(cfd, F_SETFL, O_NONBLOCK|O_CLOEXEC);
+				fcntl(cfd, F_SETFL, O_NONBLOCK);
 		} else
 
 #ifdef USE_ACCEPT4
@@ -552,12 +552,12 @@ void listener_accept(int fd)
 		 * fallback to the legacy accept() + fcntl().
 		 */
 		if (unlikely(accept4_broken ||
-			((cfd = accept4(fd, (struct sockaddr *)&addr, &laddr, SOCK_NONBLOCK|SOCK_CLOEXEC)) == -1 &&
+			((cfd = accept4(fd, (struct sockaddr *)&addr, &laddr, SOCK_NONBLOCK)) == -1 &&
 			(errno == ENOSYS || errno == EINVAL || errno == EBADF) &&
 			(accept4_broken = 1))))
 #endif
 			if ((cfd = accept(fd, (struct sockaddr *)&addr, &laddr)) != -1)
-				fcntl(cfd, F_SETFL, O_NONBLOCK|O_CLOEXEC);
+				fcntl(cfd, F_SETFL, O_NONBLOCK);
 
 		if (unlikely(cfd == -1)) {
 			switch (errno) {
@@ -603,6 +603,10 @@ void listener_accept(int fd)
 				goto stop;
 			}
 		}
+
+		/* we don't want to leak the FD upon reload if it's in the master */
+		if (unlikely(master == 1))
+			fcntl(cfd, F_SETFD, FD_CLOEXEC);
 
 		if (unlikely(cfd >= global.maxsock)) {
 			send_log(p, LOG_EMERG,