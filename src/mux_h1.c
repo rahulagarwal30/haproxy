rahulagarwal30/haproxy

commit 3815b227fe5e820f1539ecce034937c50f1c2cda

MEDIUM: mux-h1: implement true zero-copy of DATA blocks
When the mux's buffer is empty and the htx area contains exactly one
data block of the same size as the requested count, then it's possible
to simply swap the caller's buffer with the mux's output buffer and
adjust offsets and length to match the entire DATA HTX block in the
middle. In this case we perform a true zero-copy operation from
end-to-end. This is the situation that happens all the time with large
files. With this change, the HTX bit rate performance catches up again
with the legacy mode (measured at 97%).

src/mux_h1.c
@@ -1356,20 +1356,47 @@ static size_t h1_process_output(struct h1c *h1c, struct buffer *buf, size_t coun
 		errflag = H1S_F_REQ_ERROR;
 	}
 
+	blk = htx_get_head_blk(chn_htx);
 
 	tmp = get_trash_chunk();
 
-	/* pre-align the output buffer like the HTX in case it's empty. In this
-	 * case since it's aligned we don't need to use the temporary trash.
+	/* Perform some optimizations to reduce the number of buffer copies.
+	 * First, if the mux's buffer is empty and the htx area contains
+	 * exactly one data block of the same size as the requested count,
+	 * then it's possible to simply swap the caller's buffer with the
+	 * mux's output buffer and adjust offsets and length to match the
+	 * entire DATA HTX block in the middle. In this case we perform a
+	 * true zero-copy operation from end-to-end. This is the situation
+	 * that happens all the time with large files. Second, if this is not
+	 * possible, but the mux's output buffer is empty, we still have an
+	 * opportunity to avoid the copy to the intermediary buffer, by making
+	 * the intermediary buffer's area point to the output buffer's area.
+	 * In this case we want to skip the HTX header to make sure that copies
+	 * remain aligned and that this operation remains possible all the
+	 * time. This goes for headers, data blocks and any data extracted from
+	 * the HTX blocks.
 	 */
 	if (!b_data(&h1c->obuf)) {
 		h1c->obuf.head = sizeof(struct htx);
+
+		if (chn_htx->used == 1 &&
+		    blk && htx_get_blk_type(blk) == HTX_BLK_DATA &&
+		    htx_get_blk_value(chn_htx, blk).len == count) {
+			void *old_area = h1c->obuf.area;
+
+			h1c->obuf.area = buf->area;
+			h1c->obuf.data = count;
+
+			buf->area = old_area;
+			buf->data = buf->head = 0;
+			total += count;
+			goto out;
+		}
 		tmp->area = h1c->obuf.area + h1c->obuf.head;
 	}
 
 	tmp->size = b_room(&h1c->obuf);
 
-	blk = htx_get_head_blk(chn_htx);
 	while (count && !(h1s->flags & errflag) && blk) {
 		struct htx_sl *sl;
 		struct ist n, v;
@@ -1535,9 +1562,10 @@ static size_t h1_process_output(struct h1c *h1c, struct buffer *buf, size_t coun
 	else
 		b_putblk(&h1c->obuf, tmp->area, tmp->data);
 
+	htx_to_buf(chn_htx, buf);
+ out:
 	if (!buf_room_for_htx_data(&h1c->obuf))
 		h1c->flags |= H1C_F_OUT_FULL;
-	htx_to_buf(chn_htx, buf);
   end:
 	return total;
 }

commit a9de9f5b37eeea198fa020927cc46b494bc4fbce

BUG/MEDIUM: mux-h1: Fix the zero-copy on output for chunked messages
The commit 3815b227f ("MEDIUM: mux-h1: implement true zero-copy of DATA blocks")
broke the output of chunked messages. When the zero-copy was performed on such
messages, no chunk size was emitted nor ending CRLF.

Now, the chunked envelope is added when necessary. We have at least the size of
the struct htx to emit it. So 40 bytes for now. It should be enough.

src/mux_h1.c
@@ -839,6 +839,34 @@ static void h1_capture_bad_message(struct h1c *h1c, struct h1s *h1s,
 			    &ctx, h1_show_error_snapshot);
 }
 
+/* Emit the chunksize followed by a CRLF in front of data of the buffer
+ * <buf>. It goes backwards and starts with the byte before the buffer's
+ * head. The caller is responsible for ensuring there is enough room left before
+ * the buffer's head for the string.
+ */
+static void h1_emit_chunk_size(struct buffer *buf, size_t chksz)
+{
+	char *beg, *end;
+
+	beg = end = b_head(buf);
+	*--beg = '\n';
+	*--beg = '\r';
+	do {
+		*--beg = hextab[chksz & 0xF];
+	} while (chksz >>= 4);
+	buf->head -= (end - beg);
+	b_add(buf, end - beg);
+}
+
+/* Emit a CRLF after the data of the buffer <buf>. The caller is responsible for
+ * ensuring there is enough room left in the buffer for the string. */
+static void h1_emit_chunk_crlf(struct buffer *buf)
+{
+	*(b_peek(buf, b_data(buf)))     = '\r';
+	*(b_peek(buf, b_data(buf) + 1)) = '\n';
+	b_add(buf, 2);
+}
+
 /*
  * Parse HTTP/1 headers. It returns the number of bytes parsed if > 0, or 0 if
  * it couldn't proceed. Parsing errors are reported by setting H1S_F_*_ERROR
@@ -1389,6 +1417,16 @@ static size_t h1_process_output(struct h1c *h1c, struct buffer *buf, size_t coun
 
 			buf->area = old_area;
 			buf->data = buf->head = 0;
+
+			/* The message is chunked. We need to emit the chunk
+			 * size. We have at least the size of the struct htx to
+			 * write the chunk envelope. It should be enough.
+			 */
+			if (h1m->flags & H1_MF_CHNK) {
+				h1_emit_chunk_size(&h1c->obuf, count);
+				h1_emit_chunk_crlf(&h1c->obuf);
+			}
+
 			total += count;
 			goto out;
 		}