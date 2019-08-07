rahulagarwal30/haproxy

commit 015e4d7d93a2a34e79bd08e832046708e5d37f1e

MINOR: stick-tables: Add peers process binding computing.
Add a list of proxies for all the stick-tables (->proxies_list struct stktable
member) so that to be able to compute the process bindings of the peers after having
parsed the configuration file.
The proxies are added to the stick-tables they reference when parsing
stick-tables lines in proxy sections, when checking the actions in
check_trk_action() and when resolving samples args for stick-tables
without checking is they are duplicates. We check only there is no loop.
Then, after having parsed everything, we add the proxy bindings to the
peers frontend bindings with stick-tables they reference.

src/action.c
@@ -56,6 +56,10 @@ int check_trk_action(struct act_rule *rule, struct proxy *px, char **err)
 		return 0;
 	}
 	else {
+		if (target->proxies_list != px) {
+			px->next_stkt_ref = target->proxies_list;
+			target->proxies_list = px;
+		}
 		free(rule->arg.trk_ctr.table.n);
 		rule->arg.trk_ctr.table.t = target;
 		/* Note: if we decide to enhance the track-sc syntax, we may be

src/sample.c
@@ -1278,6 +1278,11 @@ int smp_resolve_args(struct proxy *p)
 				break;
 			}
 
+			if (t->proxies_list != p) {
+				p->next_stkt_ref = t->proxies_list;
+				t->proxies_list = p;
+			}
+
 			free(arg->data.str.area);
 			arg->data.str.area = NULL;
 			arg->unresolved = 0;

include/types/proxy.h
@@ -395,6 +395,7 @@ struct proxy {
 	struct conn_src conn_src;               /* connection source settings */
 	enum obj_type *default_target;		/* default target to use for accepted streams or NULL */
 	struct proxy *next;
+	struct proxy *next_stkt_ref;    /* Link to the list of proxies which refer to the same stick-table. */
 
 	struct list logsrvs;
 	struct list logformat; 			/* log_format linked list */

include/types/stick_table.h
@@ -185,6 +185,7 @@ struct stktable {
 		void *p;
 	} data_arg[STKTABLE_DATA_TYPES]; /* optional argument of each data type */
 	struct proxy *proxy;      /* The proxy this stick-table is attached to, if any.*/
+	struct proxy *proxies_list; /* The list of proxies which reference this stick-table. */
 };
 
 extern struct stktable_data_type stktable_data_types[STKTABLE_DATA_TYPES];

src/cfgparse-listen.c
@@ -1749,6 +1749,12 @@ int cfg_parse_listen(const char *file, int linenum, char **args, int kwm)
 		stktable_store_name(curproxy->table);
 		curproxy->table->next = stktables_list;
 		stktables_list = curproxy->table;
+
+		/* Add this proxy to the list of proxies which refer to its stick-table. */
+		if (curproxy->table->proxies_list != curproxy) {
+			curproxy->next_stkt_ref = curproxy->table->proxies_list;
+			curproxy->table->proxies_list = curproxy;
+		}
 	}
 	else if (!strcmp(args[0], "stick")) {
 		struct sticking_rule *rule;

src/cfgparse.c
@@ -3861,6 +3861,19 @@ out_uri_auth_compat:
 		if (curproxy->table && curproxy->table->peers.p)
 			curproxy->table->peers.p->peers_fe->bind_proc |= curproxy->bind_proc;
 
+	/* compute the required process bindings for the peers from <stktables_list>
+	 * for all the stick-tables, the ones coming with "peers" sections included.
+	 */
+	for (t = stktables_list; t; t = t->next) {
+		struct proxy *p;
+
+		for (p = t->proxies_list; p; p = p->next_stkt_ref) {
+			if (t->peers.p && t->peers.p->peers_fe) {
+				t->peers.p->peers_fe->bind_proc |= p->bind_proc;
+			}
+		}
+	}
+
 	if (cfg_peers) {
 		struct peers *curpeers = cfg_peers, **last;
 		struct peer *p, *pb;

commit 79491fbd1432793058b874d249ee7c53d88b8fce

BUG/MEDIUM: stick-table: Wrong stick-table backends parsing.
When parsing references to stick-tables declared as backends, they are added to
a list of proxies (they are proxies!) which refer to this stick-tables.
Before this patch we added them to these list without checking they were already
present, making the silly hypothesis the actions/sample were checked/resolved in the same
order the proxies are parsed.

This patch implement a simple inline function to in_proxies_list() to test
the presence of a proxy in a list of proxies. We use this function when resolving
/checking samples/actions.

This bug was introduced by 015e4d7 commit.

Must be backported to 2.0.

include/proto/proxy.h
@@ -184,6 +184,21 @@ static inline int l7_status_match(struct proxy *p, int status)
 	}
 	return 0;
 }
+
+/* Return 1 if <p> proxy is in <list> list of proxies which are also stick-tables,
+ * 0 if not.
+ */
+static inline int in_proxies_list(struct proxy *list, struct proxy *proxy)
+{
+	struct proxy *p;
+
+	for (p = list; p; p = p->next_stkt_ref)
+		if (proxy == p)
+			return 1;
+
+	return 0;
+}
+
 #endif /* _PROTO_PROXY_H */
 
 /*

src/action.c
@@ -56,7 +56,7 @@ int check_trk_action(struct act_rule *rule, struct proxy *px, char **err)
 		return 0;
 	}
 	else {
-		if (target->proxies_list != px) {
+		if (!in_proxies_list(target->proxies_list, px)) {
 			px->next_stkt_ref = target->proxies_list;
 			target->proxies_list = px;
 		}

src/sample.c
@@ -1274,7 +1274,7 @@ int smp_resolve_args(struct proxy *p)
 				break;
 			}
 
-			if (t->proxies_list != p) {
+			if (!in_proxies_list(t->proxies_list, p)) {
 				p->next_stkt_ref = t->proxies_list;
 				t->proxies_list = p;
 			}